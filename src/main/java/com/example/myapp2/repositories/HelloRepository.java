bbc53d1d5a24218cc54f7c3cc3da73ada0a0ac9e
Jeremy Stern <jzstern@gmail.com>
2018-05-18 13:15:28 -0400

    for fucks sake

--- src/main/java/com/example/myapp2/repositories/HelloRepository.java
+++ src/main/java/com/example/myapp2/repositories/HelloRepository.java
@@ -1,8 +1,8 @@
-package com.example.myapp2.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import com.example.myapp2.models.Hello;
-
-public interface HelloRepository
-	extends CrudRepository<Hello, Integer> {
-}
+//package com.example.myapp2.repositories;
+//
+//import org.springframework.data.repository.CrudRepository;
+//import com.example.myapp2.models.Hello;
+//
+//public interface HelloRepository
+//	extends CrudRepository<Hello, Integer> {
+//}
