84a0929dfaab0d29d8b3dbc56844cd333b4f659a
Jeremy Stern <jzstern@gmail.com>
2018-05-18 13:49:09 -0400

    test

--- src/main/java/com/example/myapp2/models/Hello.java
+++ src/main/java/com/example/myapp2/models/Hello.java
@@ -1,26 +1,26 @@
-//package com.example.myapp2.models;
-//
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//
-//@Entity
-//public class Hello {
-//	@Id
-//	@GeneratedValue(strategy=GenerationType.IDENTITY)
-//	private int id;
-//	private String message;
-//	public int getId() {
-//		return id;
-//	}
-//	public void setId(int id) {
-//		this.id = id;
-//	}
-//	public String getMessage() {
-//		return message;
-//	}
-//	public void setMessage(String msg) {
-//		this.message = msg;
-//	}
-//}
+package com.example.myapp2.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Hello {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String message;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String msg) {
+		this.message = msg;
+	}
+}
