46e9502887f0de679d7fccf937485274b18b060b
Jeremy Stern <jzstern@gmail.com>
2018-05-18 11:30:20 -0400

    recreating git repo

---
+++ src/main/java/com/example/myapp2/repositories/HelloRepository.java
@@ -0,0 +1,8 @@
+package com.example.myapp2.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import com.example.myapp2.models.Hello;
+
+public interface HelloRepository
+	extends CrudRepository<Hello, Integer> {
+}
