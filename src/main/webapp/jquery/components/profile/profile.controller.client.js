5c4ce53a2bbe8aa6625b81e14887d2a409b20593
Jeremy Stern <jzstern@gmail.com>
2018-05-21 23:51:41 -0400

    release

--- src/main/webapp/jquery/components/profile/profile.controller.client.js
+++ src/main/webapp/jquery/components/profile/profile.controller.client.js
@@ -1,80 +1,82 @@
 (function() {
-    $(init);
+	$(main);
 
-    var globalUserId;
-    var $username;
-    var $phoneNumber;
-    var $email;
-    var $role;
-    var $birthday;
-    var updateBtn, logoutBtn;
-    var userService = new UserServiceClient();
+	var globalUserId;
+	var $username;
+	var $phoneNumber;
+	var $email;
+	var $role;
+	var $birthday;
+	var updateBtn, logoutBtn;
+	var userService = new UserServiceClient();
 
-    function init() {
-        $username = $("#username");
-        $phoneNumber = $("#phoneNumber");
-        $email = $("#email");
-        $role = $("#role");
-        $birthday = $("#birthday");
-        updateBtn = $("#update-button").click(updateProfile);
-        logoutBtn = $("#logout-button").click(logout);
+	function main() {
+		$username = $("#username");
+		$phoneNumber = $("#phoneNumber");
+		$email = $("#email");
+		$role = $("#role");
+		$birthday = $("#birthday");
+		updateBtn = $("#update-button").click(updateProfile);
+		logoutBtn = $("#logout-button").click(logout);
 
-        globalUserId = window.location.href.split('?')[1];
-        findUserById(globalUserId);
-    }
+		globalUserId = window.location.href.split('?')[1];
+		findUserById(globalUserId);
+	}
 
-    function updateProfile() {
-        userService
-          .findUserById(globalUserId)
-          .then(function(originalUser) {
-            var user = {
-              firstName: originalUser.firstName,
-              lastName: originalUser.lastName,
-              username: originalUser.username,
-              phoneNumber: $phoneNumber.val(),
-              email: $email.val(),
-              role: $role.val(),
-              birthday: $birthday.val()
-            };
+	function updateProfile() {
+		userService
+			.findUserById(globalUserId)
+			.then(function(originalUser) {
+				var user = {
+					firstName: originalUser.firstName,
+					lastName: originalUser.lastName,
+					username: originalUser.username,
+					phoneNumber: $phoneNumber.val(),
+					email: $email.val(),
+					role: $role.val(),
+					birthday: $birthday.val()
+				};
 
-            userService
-                .updateUser(globalUserId, user)
-                .then(success);
-        });
+				userService
+					.updateUser(globalUserId, user)
+					.then(success);
+			});
 
-    }
+	}
 
-    function success(response) {
-        if(!response) {
-            alert('unable to update')
-        } else {
-            alert('success');
-        }
-    }
+	function success(response) {
+		if(response === null) {
+			alert('Unable to update')
+		} else {
+			alert('Record updated successfully');
+		}
+	}
 
-    function logout() {
-        console.log('CALLING LOGOUT');
-        window.location.href = "../login/login.template.client.html";
-    }
+	function logout() {
+		console.log('CALLING LOGOUT');
+		window.location.href = "../login/login.template.client.html";
+	}
 
-    function findUserById(userId) {
-        userService
-            .findUserById(userId)
-            .then(renderUser);
-    }
+	function findUserById(userId) {
+		userService
+			.findUserById(userId)
+			.then(renderUser);
+	}
 
-    function renderUser(user) {
-        var bday = new Date(user.birthday);
-        var year = user.birthday.substring(0, 4);
-        var month = bday.getMonth();
-        if (month.toString().length == 1) month = '0' + month;
-        var day = bday.getDay();
-        if (day.toString().length == 1) day = '0' + day;
+	function renderUser(user) {
+		var bday = new Date(user.birthday);
+		var year = user.birthday.substring(0, 4);
+		var month = bday.getMonth();
+		if (month.toString().length == 1) month = '0' + month;
+		var day = bday.getDay();
+		if (day.toString().length == 1) day = '0' + day;
 
-        $("#username").val(user.username);
-        $("#phoneNumber").val(user.phoneNumber);
-        $("#email").val(user.email);
-        $("#role").val(user.role);
-        $("#birthday").val(year + '-' + month + '-' + day);
-    }
+		console.log(year + '-' + month + '-' + day);
+
+		$("#username").val(user.username);
+		$("#phoneNumber").val(user.phoneNumber);
+		$("#email").val(user.email);
+		$("#role").val(user.role);
+		$("#birthday").val(year + '-' + month + '-' + day);
+	}
 })();
--- src/main/webapp/jquery/components/register/register.controller.client.js
+++ src/main/webapp/jquery/components/register/register.controller.client.js
@@ -1,58 +1,59 @@
-(function () {
+(function() {
 
-  jQuery(main);
+	$(main);
 
-  var $username, $password, $verifyPassword;
-  var userService = new UserServiceClient();
+	var $username, $password, $verifyPassword;
+	var registerBtn;
+	var userService = new UserServiceClient();
 
-  function main() {
-    $('#register-button').click(register);
-  }
+	function main() {
+		registerBtn = $('#register-button').click(register);
+	}
 
-  function register() {
-    $username = $('#usernameFld').val();
-    $password = $('#passwordFld').val();
-    $verifyPassword = $('#verifyPasswordFld').val();
+	function register() {
+		$username = $('#usernameFld').val();
+		$password = $('#passwordFld').val();
+		$verifyPassword = $('#verifyPasswordFld').val();
 
-    console.log('CALLED REGISTER');
+		console.log('CALLED REGISTER');
 
-    if ($password !== $verifyPassword) {
-      alert('Passwords must match');
-      clearForm;
-    }
-    else if ($username === "") {
-      alert('Username cannot be blank')
-    }
-    else if ($password === "") {
-      alert('Password cannot be blank')
-    }
-    else {
-      userService
-        .findByUsername($username)
-        .then(tryRegistration);
-    }
-  }
+		if ($password !== $verifyPassword) {
+			alert('Passwords must match');
+			clearForm;
+		}
+		else if ($username === "") {
+			alert('Username cannot be blank')
+		}
+		else if ($password === "") {
+			alert('Password cannot be blank')
+		}
+		else {
+			userService
+				.findByUsername($username)
+				.then(tryRegistration);
+		}
+	}
 
-  function tryRegistration(receivedUser) {
-    if (!receivedUser.username) {
-      var user = new User();
-      user.setUsername($username);
-      user.setPassword($password);
+	function tryRegistration(receivedUser) {
+		if (!receivedUser.username) {
+			var user = new User();
+			user.setUsername($username);
+			user.setPassword($password);
 
-      userService
-        .register(user)
-        .then(function(user) {
-          window.location.href = '../profile/profile.template.client.html?' + user.id;
-        })
-    } else {
-      alert('Sorry, that username is taken. Please choose another');
-      clearForm;
-    }
-  }
+			userService
+				.register(user)
+				.then(function(user) {
+					window.location.href = '../profile/profile.template.client.html?' + user.id;
+				})
+		} else {
+			alert('Sorry, that username is taken. Please choose another');
+			clearForm;
+		}
+	}
 
-  function clearForm() {
-    $('#username').val('');
-    $('#password').val('');
-    $('#verifyPassword').val('');
-  }
+	function clearForm() {
+		$('#username').val('');
+		$('#password').val('');
+		$('#verifyPassword').val('');
+	}
 });
